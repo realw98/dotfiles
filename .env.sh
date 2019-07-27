05c2062013a667c4ec869be78c41988de00bd53a
realw98 <realw98@gmail.com>
2019-07-27 19:23:43 +0000

    .env.sh

--- .env.sh
+++ .env.sh
@@ -4,8 +4,8 @@ export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
 export MAKEOPTS="-j5"
-export CFLAGS="-march=native"
+export CFLAGS="-march=native -O2 -pipe"
 export CPPFLAGS=$CFLAGS
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export MANPATH="/usr/local/man:$MANPATH"
 
