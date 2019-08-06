d5cda988db07f1962b144bea398a4050978d10bd
realw98 <realw98@gmail.com>
2019-08-06 14:00:44 +0000

    welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-bgFirst=$(tput setab 1) # red
-bgSecond=$(tput setab 3) # yellow
+
+colorzFirst=(1 1 2 2 4 4)
+colorzSecond=(3 5 3 6 5 6)
+arrayln=${#colorzFirst[*]}
+rnd=$((RANDOM % arrayln))
+cfirst=${colorzFirst[$rnd]}
+csecond=${colorzSecond[$rnd]}
+
+bgFirst=$(tput setab ${cfirst})
+bgSecond=$(tput setab ${csecond})
 fgBlack=$(tput setaf 0) # black
-fgFirst=$(tput setaf 1) # red
-fgSecond=$(tput setaf 3) # yellow
+fgFirst=$(tput setaf ${cfirst})
+fgSecond=$(tput setaf ${csecond})
 txBold=$(tput bold)   # bold
 txDim=$(tput dim)
 txReset=$(tput sgr0)   # reset attributes
 
+uptime=$(uptime -s)
 
-echo -ne "${fgFirst}${txDim}]]${txReset}"
+echo -ne "${fgFirst}${txDim}]${txReset}"
 echo -ne "${fgFirst}]]${txReset}"
 
 echo -ne "${bgFirst}${fgBlack}]]${txReset}"
@@ -18,17 +27,14 @@ echo -ne "${bgFirst}${fgSecond}${txDim}]]${txReset}"
 echo -ne "${bgFirst}${fgSecond}]]${txReset}"
 
 echo -ne "${bgSecond}${fgFirst}]]"
-echo -ne "${fgBlack}  WELCOME  "
+echo -ne "${fgBlack}  Uptime since ${uptime}  "
 echo -ne "${bgSecond}${fgFirst}[["
 
 echo -ne "${bgFirst}${fgSecond}[[${txReset}"
 echo -ne "${bgFirst}${fgSecond}${txDim}[[${txReset}"
 echo -ne "${bgFirst}${fgBlack}[[${txReset}"
 echo -ne "${fgFirst}[[${txReset}"
-echo -ne "${fgFirst}${txDim}[[${txReset}"
+echo -ne "${fgFirst}${txDim}[${txReset}"
 
 echo -e "${txReset}"
-echo -ne "${fgSecond}"
-uptime
-echo -ne "${txReset}"
 
