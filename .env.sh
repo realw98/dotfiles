76038395dafb41fe12ab700e0f77368686c61a15
realw98 <realw98@gmail.com>
2021-09-27 21:23:11 +0700

    aliases & env

--- .env.sh
+++ .env.sh
@@ -1,11 +1,9 @@
-
 export LANG=ru_RU.UTF-8
 export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
-export MAKEOPTS="-j5"
 export CFLAGS="-march=native -O2 -pipe"
 export CPPFLAGS=$CFLAGS
-export PATH="~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 export MANPATH="/usr/local/man:$MANPATH"
 
