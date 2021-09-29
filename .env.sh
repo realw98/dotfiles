9303c7faaddbb900bfcdd24e9e41e6ecde19eaf1
realw98 <realw98@gmail.com>
2021-09-29 16:46:29 +0700

    env & aliases

--- .env.sh
+++ .env.sh
@@ -5,5 +5,4 @@ export PAGER=less
 export CFLAGS="-march=native -O2 -pipe"
 export CPPFLAGS=$CFLAGS
 export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
-export MANPATH="/usr/local/man:$MANPATH"
 
