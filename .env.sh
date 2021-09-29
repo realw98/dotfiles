21e82eb28aedfdcb71134edeb02191f5e546743f
Root <root@eight.ldomain>
2021-09-29 16:37:49 +0700

    freebsd fixes [2]

--- .env.sh
+++ .env.sh
@@ -1,9 +1,5 @@
-export LANG=ru_RU.UTF-8
 export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
-export CFLAGS="-march=native -O2 -pipe"
-export CPPFLAGS=$CFLAGS
 export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
-export MANPATH="/usr/local/man:$MANPATH"
 
