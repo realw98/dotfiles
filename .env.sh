bfdeee376cf948c94e1e4dbde8a0ce2919ca9ef1
root <realw98@gmail.com>
2023-09-09 13:05:32 +0000

    merge

--- .env.sh
+++ .env.sh
@@ -6,6 +6,6 @@ export PAGER=less
 export MAKEOPTS="-j5"
 export CFLAGS="-march=native -O2 -pipe"
 export CPPFLAGS=$CFLAGS
-export PATH="~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
 export MANPATH="/usr/local/man:$MANPATH"
 
