2048dd2de75954509319a4e6dbd3e10181fd4be6
realw98 <realw98@gmail.com>
2023-10-03 17:39:32 +0500

    nvchad

--- .env.sh
+++ .env.sh
@@ -1,5 +1,6 @@
 export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
+export ORIGINAL_PATH=$PATH
 export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
--- setup.sh
+++ setup.sh
@@ -4,15 +4,24 @@
 cd $HOME
 
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
 mkdir -p ~/.vim/autoload/
 cd ~/.vim/autoload/
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget -O plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd ~
+mkdir -p ~/.tmux/plugins
 git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cd ~
+mkdir -p ~/.config/nvim
+git clone --depth=1 https://github.com/NvChad/NvChad.git ~/.config/nvim
 
 ln -s ~/dotfiles/.Xresources ~/.Xresources
 ln -s ~/dotfiles/.zshrc ~/.zshrc 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.config/tmux/tmux.conf
-cp ~/.bashrc ~/.bashrc.orig
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+mv ~/.bashrc ~/.bashrc.orig
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+mv ~/.bash_profile ~/.bash_profile.orig
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
