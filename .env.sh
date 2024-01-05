98a6a851f4a7d2cf309616943af2057b07560dad
realw98 <realw98@gmail.com>
2024-01-05 15:07:28 +0500

    alaises

--- .env.sh
+++ .env.sh
@@ -5,5 +5,5 @@ export CFLAGS="-march=native -O2 -pipe"
 export CPPFLAGS=$CFLAGS
 export MANPATH="/usr/local/man:$MANPATH"
 export ORIGINAL_PATH=$PATH
-export PATH="~/bin:$ORIGINAL_PATH"
+export PATH="~/.local/bin:$ORIGINAL_PATH"
 
