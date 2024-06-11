360636bee46943aabe3bea13c97406c3c0900096
realw98 <realw98@gmail.com>
2024-06-11 20:44:37 +0700

    hyprland fixes

--- hypr/scripts/xdg.sh
+++ hypr/scripts/xdg.sh
@@ -4,6 +4,6 @@ killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 echo "Starting xdg-desktop-portal-hyprland" >> xdg-desktop-portal-hyprland.log
-/usr/lib/xdg-desktop-portal-hyprland >> xdg-desktop-portal-hyprland.log 2>&1 &
+/usr/local/libexec/xdg-desktop-portal-hyprland >> xdg-desktop-portal-hyprland.log 2>&1 &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+/usr/local/libexec/xdg-desktop-portal &
