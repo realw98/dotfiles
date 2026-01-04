75a7b67b8826b857cf131272f9f600cded01fb6f
realw98 <realw98@gmail.com>
2026-01-04 17:57:24 +0700

    hyprland fixed

--- hypr/scripts/xdg.sh
+++ hypr/scripts/xdg.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 sleep 1
 killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-echo "Starting xdg-desktop-portal-hyprland" >> xdg-desktop-portal-hyprland.log
-/usr/lib/xdg-desktop-portal-hyprland >> xdg-desktop-portal-hyprland.log 2>&1 &
-sleep 2
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
+
+echo "Starting xdg-desktop-portal-hyprland"
+
+sleep 1
+/usr/lib/xdg-desktop-portal-hyprland &
+#sleep 1
+#/usr/lib/xdg-desktop-portal-kde &
+sleep 1
 /usr/lib/xdg-desktop-portal &
