4690d53c2dbd3a13918103e866ba5bd213a3a68b
realw98 <realw98@gmail.com>
2026-01-05 13:13:36 +0700

    hyprfixes

--- hypr/scripts/xdg.sh
+++ hypr/scripts/xdg.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-kde
 killall -e xdg-desktop-portal-gtk
@@ -7,9 +6,9 @@ killall -e xdg-desktop-portal
 
 echo "Starting xdg-desktop-portal-hyprland"
 
-sleep 1
+sleep 0.5
 /usr/lib/xdg-desktop-portal-hyprland &
-#sleep 1
-#/usr/lib/xdg-desktop-portal-kde &
-sleep 1
+sleep 0.5
+/usr/lib/xdg-desktop-portal-kde &
+sleep 0.5
 /usr/lib/xdg-desktop-portal &
