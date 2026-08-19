3fac545ddff74d13e96043b7075c76e5f34f107d
realw98 <realw98@gmail.com>
2026-08-19 16:57:25 +0700

    some more fixes

--- setup.sh
+++ setup.sh
@@ -1,32 +1,28 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env sh
 
 cd $HOME
 
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 mkdir -p ~/.vim/autoload/
-cd ~/.vim/autoload/
-wget -O plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget -O ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cd ~
+
 mkdir -p ~/.tmux/plugins
 git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cd ~
+mkdir -p ~/.config/tmux
+ln -s ~/dotfiles/.tmux.conf ~/.config/tmux/tmux.conf
 
+ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-
-mkdir -p ~/.config/tmux
-ln -s ~/dotfiles/.tmux.conf ~/.config/tmux/tmux.conf
-ln -s ~/dotfiles/nvim ~/.config/nvim
-
-cp ~/dotfiles/.aliases ~/.aliases
 
 mv ~/.bashrc ~/.bashrc.orig
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 
 mv ~/.bash_profile ~/.bash_profile.orig
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+
+cp ~/dotfiles/.aliases ~/.aliases
--- xsetup.sh
+++ xsetup.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+ln -s ~/dotfiles/.Xresources ~/.Xresources
 ln -sT ~/dotfiles/dunst ~/.config/dunst
 ln -sT ~/dotfiles/hypr ~/.config/hypr
 ln -sT ~/dotfiles/waybar ~/.config/waybar
 ln -sT ~/dotfiles/mako ~/.config/mako
 ln -sT ~/dotfiles/niri ~/.config/niri
+mkdir -p ~/.config/icewm
+ln -s ~/dotfiles/icewm/keys ~/.config/icewm/keys
+ln -s ~/dotfiles/icewm/preferences ~/.config/icewm/preferences
+ln -s ~/dotfiles/icewm/startup ~/.config/icewm/startup
